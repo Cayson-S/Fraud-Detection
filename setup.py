ccd97ffc60ee9ad74c0b12abfc5c7df6e178e5c6
Cayson <crs9g@mtmail.mtsu.edu>
2023-02-27 19:10:59 -0600

    create a new cookiecutter repository

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The project fufills the requirements set by the data science capstone. It looks to predict fraud from transaction data.',
+    author='Cayson Seipel',
+    license='',
+)
