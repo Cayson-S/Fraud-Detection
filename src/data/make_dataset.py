3c1178b02e6ac52782229ed971f33fb57aa1ef50
Cayson <crs9g@mtmail.mtsu.edu>
2023-04-16 22:15:13 -0500

    finished feauture engineering.

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -30,24 +30,3 @@ class CleanData:
         data[cols].apply(pd.to_numeric)
 
         return data
-    
-    def convert_to_date(data: pd.DataFrame, col: str) -> pd.Dataframe:
-        # This function converts a column into a date field (an hour and a day column)
-        # The original column is an integer type representing the hours since the beginning of the month
-        # :param data: the dataset
-        # :type data: pd.Dataframe
-        # :param col: column location to be converted to a date field
-        # :type col: string 
-        # :returns: dataframe
-        # :rtype: pd.dataframe 
-
-        # determine the day and hour of the transaction
-        # Add the data to new columns
-        for i in range(len(data)):
-            data["day"][i] = data[col][i]//24
-            data["hour"][i] = data[col][i] % 24
-        
-        # Remove the old column from the dataset
-        data.drop(col, index = 1, inplace = True)
-
-        return data
